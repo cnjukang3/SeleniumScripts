dfe4b92391f39f8ec3dfefe4d473809b8700afb7
cnjukang3 <celestine.njukang@gmail.com>
2020-01-24 11:50:01 -0500

    First java application

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+    public static void main(String[] args){
+	   //print some text
+	   System.out.println("Hello World!");
+	   System.out.println("Welcome to java programming");
+	}
+}
+    
+
